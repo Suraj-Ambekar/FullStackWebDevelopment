7ce65a94d9931a7765eee31cd1555b52abf6d15b
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-22 16:18:14 +0530

    learned about differnts type of op method

---
+++ 5 JavaScript/4 how to output js/script.js
@@ -0,0 +1,21 @@
+// alert("suraj");
+
+// console.log(prompt("hey there wupps?"));
+
+// console.log(confirm("are u there>"));
+
+// console.log("hey there");
+// console.warn("its a warning");
+// console.error("it's an error");
+
+// console.log("%c hey tehre", "color:red; font-size: 50px;");
+
+// tasks
+
+/*alert("Js is cool");
+
+console.log(prompt("what country are you from?", "name:"));
+
+console.log(confirm("continue learning"));
+
+console.log("I am learnign js");*/
