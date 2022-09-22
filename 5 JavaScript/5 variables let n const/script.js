00c9d4c9d6b50dd214b72ee6c9b72c312f54e91f
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-22 16:54:03 +0530

    learned about variables let and const

---
+++ 5 JavaScript/5 variables let n const/script.js
@@ -0,0 +1,24 @@
+// let channelName;
+
+// channelName = "hi suraj";
+
+// console.log(channelName);
+
+// let suraj =1, kiran =2, mummy =3; o
+
+// const suraj= 1;
+
+
+// tasks
+/*let favFood ="Pizza", favColor= "Red", favDrink= "lemonade";
+
+console.log(favDrink);
+
+favFood = "Pasta";
+
+const birthDay = "12th March 2000";
+
+console.log(birthDay);*/
+
+
+
