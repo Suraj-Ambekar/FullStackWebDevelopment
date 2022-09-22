34ce8bdff4d86e771ad309d216da94b1fd84ca9d
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-22 15:47:37 +0530

    learned about syntax comments

---
+++ 5 JavaScript/3 js syntax and comment/script.js
@@ -0,0 +1,24 @@
+alert("hello world"); //always use semicolon
+
+// for single line comment
+
+/* for 
+double
+line comment */
+
+//for good comment 
+
+/**
+ * 
+ * @param {number} num to find a even or add number
+ */
+function isEven(num){
+    if(num%2 ==0){
+        console.log("even number");
+    }
+    else{
+        console.log("odd number");
+    }
+}
+
+isEven(2);
