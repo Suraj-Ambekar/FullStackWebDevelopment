7aa0ca6d8febf597fcbc9e1d00fe8f554423f838
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-26 11:47:19 +0530

    learne about arithmetic operators

---
+++ 5 JavaScript/7 arithmetic operators/script.js
@@ -0,0 +1,61 @@
+//Arithmetic operators
+
+console.log(5+2);   //addition
+console.log(5-2);  // sub
+console.log(5/2); //division
+console.log(5*2);//mult
+
+
+console.log(5%2); //modulus
+
+console.log(5**2); //expoention
+
+//console.log(5++); //this will show an error because we can't change directly.
+
+let a = 1;
+console.log(a++);
+console.log(a);
+// or 
+
+a++;
+console.log(a);
+
+//or 
+a--;
+console.log(a);
+
+//or 
+console.log(++a);
+
+//or 
+console.log(--a);
+
+
+
+// Tasks
+console.log("op of tasks");
+
+let num1 = 10; //creating 2 var 
+let num2 = 20;
+
+console.log(num1 + num2); // adding
+
+console.log(num1%2); //modulus
+console.log(num2%2);
+
+let total = 10;
+console.log(++total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
