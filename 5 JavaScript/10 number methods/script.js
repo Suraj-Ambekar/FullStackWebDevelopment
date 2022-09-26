094c258212f6784bae313a96b83efffe01e02285
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-26 16:45:02 +0530

    learned about different number methods

---
+++ 5 JavaScript/10 number methods/script.js
@@ -0,0 +1,25 @@
+//number methods
+
+let a = 3.1485023;
+let b = a.toFixed(2);
+console.log(b);
+
+let c = a.toPrecision(4);
+console.log(c);
+
+let d = a.toExponential();
+console.log(d);
+
+//Task
+
+let PI = 3.14159;
+a = PI.toFixed(3);
+console.log(a);
+
+num2 = PI.toPrecision(3);
+
+console.log(num2);
+
+let lightSpeed = 186000;
+a = lightSpeed.toExponential();
+console.log(a);
