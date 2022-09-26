527f5010a0197b31b5087c83789fbeb4c44436fa
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-26 17:07:03 +0530

    learned about different string uses

---
+++ 5 JavaScript/11 strings dt/script.js
@@ -0,0 +1,40 @@
+// String DT
+
+//three ways
+
+let a= "suraj"; //double quotes
+let b = 'suraj'; //signle quotes
+let c = `suraj`;  //backticks
+
+//wanna use DQ in string?
+// let a = "suraj "ambekar""; it will give you an error so 
+let d = "suraj 'ambekar'"; //it will work
+// or 
+let e = "suraj \"ambkear\""; 
+
+// same follows for single quotes.
+
+//what if if wanna use \ in sentence?
+
+let f = "suraj \\\"hi\""; //double double \\ to show one \
+
+//Tasks
+
+let s = "suraj";
+let g = 'bhagwan';
+let h = `ambekar`;
+console.log(s,g,h); //we can show three var like this.
+
+let speech = "Martin Luther King said I have a dream.";
+console.log(speech);
+
+let tip = "we can escape qotes by using \\."
+console.log(tip);
+
+
+
+
+
+
+
+
