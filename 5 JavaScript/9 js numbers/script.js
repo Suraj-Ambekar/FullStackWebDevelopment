e68212029f59d8b89e7e3ca9cfc155194e024e41
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-26 13:55:47 +0530

    learned about scientific method

---
+++ 5 JavaScript/9 js numbers/script.js
@@ -0,0 +1,21 @@
+//numbers
+
+let mill = 1e5;
+console.log(mill);
+
+let milll = 1e-5;
+console.log(milll);
+
+let score = new Number(100); 
+console.log(typeof score)
+
+// Tasks
+
+let x = 1e5;
+console.log(x);
+console.log(typeof x);
+
+let b = 984.498;
+console.log(typeof b); 
+
+
