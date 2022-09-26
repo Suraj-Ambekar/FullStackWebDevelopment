5a393aa93f2e8445c5abf2d3b03b9a367f6ecdb7
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-26 13:33:53 +0530

    learned about assignmet operators

---
+++ 5 JavaScript/8 assignmet operators/script.js
@@ -0,0 +1,47 @@
+//Assignmet operators
+
+
+let a = 10; // assigning value
+
+a += 10; // += operator
+console.log(a);
+
+a -= 10; // -= 
+console.log(a);
+
+a *= 2; // *=
+console.log(a);
+
+a /= 2; // /=
+console.log(a);
+
+a %= 3; // %=
+console.log(a);
+
+
+// TAsks
+
+let x = 30;
+x += 10; 
+console.log(x); 
+x /=2; 
+console.log(x); //guess the ans
+
+let xp = 250;
+
+xp *= 2;
+console.log(xp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
