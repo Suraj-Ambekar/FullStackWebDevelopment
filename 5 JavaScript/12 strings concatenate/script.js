9fec5aeb8cce2ab731415f65af39bac2cacb5197
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-27 11:26:46 +0530

    learned about string concatenation

---
+++ 5 JavaScript/12 strings concatenate/script.js
@@ -0,0 +1,39 @@
+// String Concatenate
+
+let a = "suraj ";
+let b = "ambekar";
+
+console.log(a+b);  // used '+' to add two strings.
+
+console.log(a+"bhagwan "+b); //in this way we can add new string at same time.
+
+let str = "10";
+let num = 5;
+console.log(str+num); //op will be '105' not 15 because js assume it as a string.
+
+// another method to to a concatination
+
+console.log(a.concat(b));
+//or
+console.log(a.concat("the don"));
+//or
+console.log(a.concat(5));
+
+//Tasks
+
+let c = "I am learning ";
+let d = "JavaScript";
+let result = c+d;
+console.log(result);
+
+console.log(c.concat(d));
+
+
+
+
+
+
+
+
+
+
