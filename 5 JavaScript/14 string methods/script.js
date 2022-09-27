da2426d3c9d8099bc84d353d2b58564bfe312c6c
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-27 13:39:21 +0530

    learned about the string methods

---
+++ 5 JavaScript/14 string methods/script.js
@@ -0,0 +1,29 @@
+// String methods
+
+let a = "JavaScript is cool";
+
+console.log(a.length);
+console.log(a.trim());
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.toLocaleUpperCase());
+console.log(a.indexOf("a"));
+console.log(a.lastIndexOf("a"));
+console.log(a.slice(0,10));
+console.log(a.replace("cool", "like cool dude."));
+console.log(a.charAt(5));
+
+
+//Tasks
+
+let x = "I am watching Dev Dreamer";
+
+console.log(x.toUpperCase());
+console.log(x.slice(14,25));
+
+
+
+
+
+
+
