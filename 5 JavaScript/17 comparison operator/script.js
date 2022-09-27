cdb0fb7cbe5965921d2ee85645a9e0c7c33ed29b
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-27 23:30:07 +0530

    learned about comparison operator

---
+++ 5 JavaScript/17 comparison operator/script.js
@@ -0,0 +1,36 @@
+// Comparison Operator
+
+//Relational operator
+
+ let x =8;
+
+ let y = '8';
+
+ console.log( x< 3);
+
+ console.log( x> 3);
+
+ console.log( x<= 3);
+ 
+ console.log( x<= 3);
+
+
+ // Abstrac or loose operator
+
+ console.log( x== 3);
+
+ console.log( x!= 3);
+
+// Strict operator
+
+console.log( x=== 3);
+
+console.log( x !== y);
+
+// we can compare strings to 
+
+let a = "suraj";
+let b = "Surya";
+
+console.log( a<b);
+console.log(a>b);
