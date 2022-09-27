0877ab4648a49d5ccb76c4cd2dcaf348f381ff57
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-09-27 18:23:47 +0530

    learned about math objects

---
+++ 5 JavaScript/16 math object/script.js
@@ -0,0 +1,31 @@
+// Math Object 
+
+const PI = Math.PI;
+console.log(PI);
+
+let min = Math.min(1,57,37,5);
+console.log(min);
+
+let max = Math.max(64,88,57,27,44);
+console.log(max);
+
+let pow = Math.pow(5,3);
+console.log(pow);
+
+let random = Math.random().toFixed(1)*10;
+console.log(random);
+
+console.log(Math.floor(5.1));
+
+console.log(Math.round(5.7));
+
+//Tasks
+
+let num1 = Math.random().toFixed(1)*10;
+let num2 = Math.random().toFixed(1)*10;
+let num3 = Math.random().toFixed(1)*10;
+
+console.log(`${num1}${num2}${num3}`);
+
+let largest = Math.max(66,29,76);
+console.log(largest);
