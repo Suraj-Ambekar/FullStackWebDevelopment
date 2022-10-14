808d5617ca99ded0ae2d83decbfa360d58ee788b
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-10-14 18:34:34 +0530

    learned about array's iterator methods forEach(), map() , filter() and few of them are remaining

---
+++ 5 JavaScript/29 Array iterator/script.js
@@ -0,0 +1,43 @@
+//Array iterator
+
+let supers = ['super','man','spider','mans','bat','manss','don'];
+
+//forEach()
+
+supers.forEach(
+    function (value, index, array) {
+        console.log(`The index of ${value} is ${index}`);
+        console.log(array);        
+    }
+);
+
+
+//map()
+
+function upperCase(x){
+    return x.toUpperCase();
+}
+
+let uCase = supers.map(upperCase);
+console.log(uCase);
+
+let numArr = [1,2,3,4,5,6];
+
+function double(y){
+    return y*2;
+}
+
+let newNumArr = numArr.map(double);
+console.log(newNumArr);
+
+//filter()
+
+let num = [1,2,3,4,5,6,7,8,9,10,12,543,72,734,733,62,26];
+
+let newNum = num.filter(
+    function (value, index, array){
+        return value % 2 === 0;
+    }  
+);
+
+console.log(newNum);
