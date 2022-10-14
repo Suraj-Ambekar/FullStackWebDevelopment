89521547ce1d960a20f96e84ae742c3071183383
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-10-14 14:27:13 +0530

    learned about arrays

---
+++ 5 JavaScript/27 Arrays/script.js
@@ -0,0 +1,37 @@
+// Arrays
+
+
+let arr = ['Suraj','Bhagwan','Ambekar'];
+
+console.log(arr);
+
+//if we want to access the value of an array
+console.log(arr[2]);
+
+//To add an value
+arr[3] = 'It is the full name';
+console.log(arr)
+
+//to delete any value
+delete arr[3];
+console.log(arr); 
+/*as you can see in the op that it is showing an empty 
+because value is deleted but the index is remaining that's 
+why it is showing this*/
+
+//we can add any value to that position 
+arr[3] = 'yes';
+console.log(arr);
+
+//Tasks
+
+let country = ['India','Australia','England'];
+
+country[3] = 'New Zeland';
+console.log(country);
+
+let colors = ['red','pink','green',['yello','black','orange']];
+
+console.log(colors[3][2]);
+ 
+
