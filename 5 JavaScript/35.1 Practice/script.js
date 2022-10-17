6419966e71f9a6c95aa23d5293680476b0c54cf1
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-10-17 14:38:00 +0530

    learned about op methods, how to add css, and types of alert boxes

--- 5 JavaScript/35.1 Practice/script.js
+++ 5 JavaScript/35.1 Practice/script.js
@@ -10,6 +10,19 @@ comment*/
  * comment
  */
 
+//Types of output method
+console.log("hey there!");
+console.warn("It's warning");
+console.error("it's an error");
+console.clear();
+
+//How to add css to javascript
+console.log("%c adding css", "color: red; font-size: 20px;");
+
+//Types of alert boxes
+alert("hie");  //it has only ok button
+prompt("r u there?", "yes or no");  // we can ask to type
+confirm("r u there"); // it has ok and cancel button
 
 
 
