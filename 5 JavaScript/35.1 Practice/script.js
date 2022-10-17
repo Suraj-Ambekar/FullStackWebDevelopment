0d51eeb80051c0a79f85ab6c086b9a0e3575517c
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-10-17 17:20:31 +0530

    revised date and math object

--- 5 JavaScript/35.1 Practice/script.js
+++ 5 JavaScript/35.1 Practice/script.js
@@ -110,50 +110,89 @@ comment*/
 
 //String
 //different type to assign string to variable
-let k = "Suraj";
-console.log(k);
-k = 'Bhagwan';
-console.log(k);
-k = `Ambekar`;
-console.log(k);
+// let k = "Suraj";
+// console.log(k);
+// k = 'Bhagwan';
+// console.log(k);
+// k = `Ambekar`;
+// console.log(k);
 
 // k = "suraj"sura""; this will show an error
 //but to write like about use following
-k = "suraj \"suraj\""; //same follows for other
-console.log(k); 
+// k = "suraj \"suraj\""; //same follows for other
+// console.log(k); 
 
 // k = 'suraj \'; this will show an error
 // but to write this
-k = 'suraj \\'; //it will give you only one slash
-console.log(k);
+// k = 'suraj \\'; //it will give you only one slash
+// console.log(k);
 
 //different types of using String concatination
-let l = 'hii';
-console.log(k+l);
-console.log(k+'hey '+l);
-console.log(k.concat(l));
-console.log(k.concat('hii'));
+// let l = 'hii';
+// console.log(k+l);
+// console.log(k+'hey '+l);
+// console.log(k.concat(l));
+// console.log(k.concat('hii'));
 
 //Template literals (updated in ES6)
 //it is new way of string a string
-k = `suraj`;
-console.log(k);
-//another use of this is we can do following
-console.log(`hey ${k}`); //only available in this ``
-console.log(`hey
-${k}
-, how are you?`); //we can you like this write in different line
+// k = `suraj`;
+// console.log(k);
+// //another use of this is we can do following
+// console.log(`hey ${k}`); //only available in this ``
+// console.log(`hey
+// ${k}
+// , how are you?`); //we can you like this write in different line
 
 //String methods
-k = 'JavaScript is cool';
-console.log(k.length); //to find length of the string
-console.log(k.trim()); //it will trim befor and after spaces if present to check add spaces to string
-console.log(k.toLocaleLowerCase(),k.toUpperCase()); //To make in upper and lower case
-console.log(k.indexOf('a')); //it gives starting index of 'a' letter
-console.log(k.lastIndexOf('a')); //it gives last index of 'a' letter
-console.log(k.slice(0,10)); //to take out what we wanted
-console.log(k.replace('cool','the coolest'));
-console.log(k.charAt('5')); //it gives what is present at 5th index
+// k = 'JavaScript is cool';
+// console.log(k.length); //to find length of the string
+// console.log(k.trim()); //it will trim befor and after spaces if present to check add spaces to string
+// console.log(k.toLocaleLowerCase(),k.toUpperCase()); //To make in upper and lower case
+// console.log(k.indexOf('a')); //it gives starting index of 'a' letter
+// console.log(k.lastIndexOf('a')); //it gives last index of 'a' letter
+// console.log(k.slice(0,10)); //to take out what we wanted
+// console.log(k.replace('cool','the coolest'));
+// console.log(k.charAt('5')); //it gives what is present at 5th index
+
+//Date object
+//To use this we have to create date object
+let date = new Date(); //created date object
+console.log(date); //current date
+date = new Date(111111); //in milliseconds starts from 1 jan 1970
+console.log(date);
+date = new Date('Aug 29 2022 08:22:00');
+console.log(date); //date string
+date = new Date(1999,11,25,14,00,0,0);
+console.log(date); //component date
+
+//Date method
+//get method to get date
+date = new Date();
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getDate());
+
+//Set method to set date and it has same get method only different is use set instead of get
+date.setFullYear(2000);
+console.log(date);
+//same for all the methods
+
+//If you want a universal time 
+console.log(date.getUTCDate()); //it is universal
+
+
+//Math object
+let max = Math.max(2,35,64,46);
+console.log(max);
+
+let min = Math.min(2,343,234,3,1);
+console.log(min);
+console.log(Math.pow(5,2)); // 5 raise to 2
+console.log(Math.random().toFixed(1)*10);
+console.log(Math.floor(5.1));
+console.log(Math.round(5.8));
 
 
 
