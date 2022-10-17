69d4b7eca7863028a51d8bbdf531a45ec6fff2a3
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-10-17 14:56:35 +0530

    revised Data types

--- 5 JavaScript/35.1 Practice/script.js
+++ 5 JavaScript/35.1 Practice/script.js
@@ -25,11 +25,38 @@ comment*/
 // confirm("r u there"); // it has ok and cancel button
 
 //types of variables
-let type1; //undefined
-let type2 = 'hello world'; //defined
+// let type1; //undefined
+// let type2 = 'hello world'; //defined
 
-var type0 = 'Old type to declare a varible';
-const type3 = 'This is constant keyword that is not gonna change';
+// var type0 = 'Old type to declare a varible';
+// const type3 = 'This is constant keyword that is not gonna change';
+
+//Data types
+//There are two types of DT - primitive and non-primitive
+//--Primitive--
+let a = 7; //number
+let b = 0.8; //number
+let c = 'string';
+let d = "string";
+console.log(1<100); // Boolean true of false
+let e = 9999999999999999999n; //BigInt
+let f; //undefined
+//NUll
+
+//--Non-primitives--
+let arr = ['Suraj','Bhagwan','Ambekar']; //array
+
+function func(){
+    console.log("it's a function");
+}
+func();
+
+let obj = {   //It's an object
+    name: 'suraj',
+    year: 2000,
+    age: 22,
+}
+
 
 
 
