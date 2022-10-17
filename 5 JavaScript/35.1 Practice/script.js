ac309104c565ee0453cb7876f80a430581b0b5c8
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-10-17 14:45:09 +0530

    revised different types of variables

--- 5 JavaScript/35.1 Practice/script.js
+++ 5 JavaScript/35.1 Practice/script.js
@@ -11,18 +11,26 @@ comment*/
  */
 
 //Types of output method
-console.log("hey there!");
-console.warn("It's warning");
-console.error("it's an error");
-console.clear();
+// console.log("hey there!");
+// console.warn("It's warning");
+// console.error("it's an error");
+// console.clear();
 
 //How to add css to javascript
-console.log("%c adding css", "color: red; font-size: 20px;");
+// console.log("%c adding css", "color: red; font-size: 20px;");
 
 //Types of alert boxes
-alert("hie");  //it has only ok button
-prompt("r u there?", "yes or no");  // we can ask to type
-confirm("r u there"); // it has ok and cancel button
+// alert("hie");  //it has only ok button
+// prompt("r u there?", "yes or no");  // we can ask to type
+// confirm("r u there"); // it has ok and cancel button
+
+//types of variables
+let type1; //undefined
+let type2 = 'hello world'; //defined
+
+var type0 = 'Old type to declare a varible';
+const type3 = 'This is constant keyword that is not gonna change';
+
 
 
 
