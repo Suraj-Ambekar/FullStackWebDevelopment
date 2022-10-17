e712c8fbb9c273f395797e1c0922497296abde33
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-10-17 16:36:12 +0530

    revised string, str method, template literals

--- 5 JavaScript/35.1 Practice/script.js
+++ 5 JavaScript/35.1 Practice/script.js
@@ -88,25 +88,75 @@ comment*/
 // console.log(i);
 
 //Numbers
-let j = 1e6; // 1and 6 zeros 1000000
-console.log(j);
-j= 1e-6; // 0.000001
-console.log(j);
+// let j = 1e6; // 1and 6 zeros 1000000
+// console.log(j);
+// j= 1e-6; // 0.000001
+// console.log(j);
 
-j = new Number(180); // We can create it as a number method
+// j = new Number(180); // We can create it as a number method
 
 //Number methods
-j = 83.980493803;
-j = j.toFixed(3);  //How many decimal number we want after a '.'
-console.log(j);
+// j = 83.980493803;
+// j = j.toFixed(3);  //How many decimal number we want after a '.'
+// console.log(j);
 
-j = 83.980493803;
-j = j.toPrecision(3);
-console.log(j);  //it gives you number from starting regardless of point .
+// j = 83.980493803;
+// j = j.toPrecision(3);
+// console.log(j);  //it gives you number from starting regardless of point .
 
-j = 83.980493803;
-j = j.toExponential();
-console.log(j);  //It gives you scientif exponential number
+// j = 83.980493803;
+// j = j.toExponential();
+// console.log(j);  //It gives you scientif exponential number
+
+//String
+//different type to assign string to variable
+let k = "Suraj";
+console.log(k);
+k = 'Bhagwan';
+console.log(k);
+k = `Ambekar`;
+console.log(k);
+
+// k = "suraj"sura""; this will show an error
+//but to write like about use following
+k = "suraj \"suraj\""; //same follows for other
+console.log(k); 
+
+// k = 'suraj \'; this will show an error
+// but to write this
+k = 'suraj \\'; //it will give you only one slash
+console.log(k);
+
+//different types of using String concatination
+let l = 'hii';
+console.log(k+l);
+console.log(k+'hey '+l);
+console.log(k.concat(l));
+console.log(k.concat('hii'));
+
+//Template literals (updated in ES6)
+//it is new way of string a string
+k = `suraj`;
+console.log(k);
+//another use of this is we can do following
+console.log(`hey ${k}`); //only available in this ``
+console.log(`hey
+${k}
+, how are you?`); //we can you like this write in different line
+
+//String methods
+k = 'JavaScript is cool';
+console.log(k.length); //to find length of the string
+console.log(k.trim()); //it will trim befor and after spaces if present to check add spaces to string
+console.log(k.toLocaleLowerCase(),k.toUpperCase()); //To make in upper and lower case
+console.log(k.indexOf('a')); //it gives starting index of 'a' letter
+console.log(k.lastIndexOf('a')); //it gives last index of 'a' letter
+console.log(k.slice(0,10)); //to take out what we wanted
+console.log(k.replace('cool','the coolest'));
+console.log(k.charAt('5')); //it gives what is present at 5th index
+
+
+
 
 
 
