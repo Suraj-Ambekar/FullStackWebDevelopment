635fbfd15c7ecc9a0efb79480bddf45cbc7ec040
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-11-28 15:17:51 +0530

    learned about how to import and export modules from one js file to another

---
+++ 5 JavaScript/61 Import and Export/dogs.js
@@ -0,0 +1,11 @@
+//If you want to export one thing
+const dogss = ['Rocky','Great den','Fluffy'];
+
+export default dogss;
+
+//If you want to export multiple thing
+
+export const dogs = ['Rocky','Great den','Fluffy'];
+export const nDogs = dogs.length;
+
+
---
+++ 5 JavaScript/61 Import and Export/script.js
@@ -0,0 +1,12 @@
+// Importing and Exporting
+
+//If you want to import one thing
+import dogss from './dogs.js'
+
+console.log(`My dogs are ${dogss}`)
+
+
+// If you want to import multiple things
+import {dogs , nDogs} from './dogs.js'
+console.log(nDogs);
+console.log(dogs);
