bcc3e569e4d0d97893ef19f1fc3c3f2542e25d34
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-12-12 11:56:18 +0530

    learned about click event

--- 6 React/learnreactcomponents/src/Content.js
+++ 6 React/learnreactcomponents/src/Content.js
@@ -5,12 +5,25 @@ const Content = ()=>{
         let int = Math.floor(Math.random()*3);
         return arr[int];
       }
+    
+    function clickEvent(){
+        alert('Hello');
+    }
+    function clickEvent2(name){
+        alert(`${name} clicked it.`);
+    }
+    function clickEvent3(e){
+        console.log(e.target);
+    }
     return (
         <main>
             <p>
                 {/* here we have showing the result of generated name */}
                 Hello {newName()}
             </p>
+            <button onClick={clickEvent}>Click me</button>
+            <button onClick={() => clickEvent2('Hello')}>Click me</button>
+            <button onClick={(e) => clickEvent3(e)}>Click me</button>
         </main>
         
     );
