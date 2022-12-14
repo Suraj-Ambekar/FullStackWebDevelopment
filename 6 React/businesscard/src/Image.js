b74869650ab070ea479266c0dd0ae3a9f79cfe88
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-12-14 14:58:17 +0530

    created basic stucture of card

---
+++ 6 React/businesscard/src/Image.js
@@ -0,0 +1,8 @@
+import logo from './photo.jpg'
+function Image(){
+    return (
+        <img src={logo} alt="profile pic" height="317px" width="317px" />
+    )
+}
+
+export default Image
---
+++ 6 React/businesscard/src/Main.js
@@ -0,0 +1,14 @@
+function Main(){
+    return(
+        <div className="main">
+            <div className="overview">
+                
+            </div>
+            <div className="about">
+
+            </div>
+        </div>
+    )
+}
+
+export default Main
