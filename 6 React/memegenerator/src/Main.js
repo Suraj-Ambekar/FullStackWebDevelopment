97370c3b3c8aa0db08337643fa437ca37e32d2ee
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-12-20 08:10:31 +0530

    started creating topText and bottomText

--- 6 React/memegenerator/src/Main.js
+++ 6 React/memegenerator/src/Main.js
@@ -4,12 +4,25 @@ import {useState} from "react"
 function Main() {
     // let newData = memesData.data.memes;
     // let randomNum = Math.floor(Math.random()* newData.length);
-    let [memeImage, setMemeImage] = useState(``)
+    let [memeImage, setMemeImage] = useState({
+        topText : "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/30b1gx.jpg",
+    })
+
+    let [allMemeData, setAllMemeImages] = useState(memesData);
 
     function getMemeImage(){
-        let newData = memesData.data.memes;
+        let newData = allMemeData.data.memes;
         let randomNum = Math.floor(Math.random()* newData.length);
-        setMemeImage(newData[randomNum].url)
+        let url = newData[randomNum].url;
+        setMemeImage(prevMeme => ({
+            ...prevMeme,
+            randomImage: url,
+        }))
+        // setMemeImage()
+        // setMemeImage(newData[randomNum].memeImage.topText)
+        // setMemeImage(newData[randomNum].memeImage.bottomText)
     }
     
     return (
@@ -20,7 +33,7 @@ function Main() {
                 <button className="btn" onClick={getMemeImage} >Get a new Meme image</button>
             </div>
             {/* <img src={getMemeImage()} alt="meme Images" className="meme-img"/> */}
-            <img src={memeImage} alt="meme Images" className="meme-img"/>
+            <img src={memeImage.randomImage} alt="meme Images" className="meme-img"/>
         </main>
     )
 }
