f30c289db456ed14d3f2e47d08b36c5ed62867ff
Suraj Ambekar <ambekarsuraj03@gmail.com>
2022-12-20 07:50:01 +0530

    created basic structure and learned some of the basic things using demo file

--- 6 React/memegenerator/src/App.js
+++ 6 React/memegenerator/src/App.js
@@ -1,14 +1,58 @@
 // import logo from './logo.svg';
 // import './App.css';
-import Navbar from "./Navbar";
-import Main from "./Main";
+// import Navbar from "./Navbar";
+// import Main from "./Main";
+import {useState} from 'react'
 
 function App() {
+  const [contact, setContact] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false
+})
+/**
+ * Challenge: Fill in the values in the markup
+ * using the properties of our state object above
+ * (Ignore `isFavorite` for now)
+ */
+let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png";
+
+function toggleFavorite() {
+    // console.log("Toggle Favorite")
+    setContact((prevContact )=> {
+      return {
+        ...prevContact,
+        isFavorite: !prevContact.isFavorite,
+      }
+    })
+}
+
   return (
-    <div className="App">
-      < Navbar />
-      < Main />
-    </div>
+    // <div className="App">
+    //   < Navbar />
+    //   < Main />
+    // </div>
+    <main>
+            <article className="card">
+                <img src="./images/user.png" className="card--image" />
+                <div className="card--info">
+                    <img 
+                        // src={`../images/star-empty.png`} 
+                        src={`../images/${starIcon}`}
+                        className="card--favorite"
+                        onClick={toggleFavorite}
+                    />
+                    <h2 className="card--name">
+                      {contact.firstName} {contact.lastName} 
+                    </h2>
+                    <p className="card--contact">{contact.phone}</p>
+                    <p className="card--contact">{contact.email}</p>
+                </div>
+                
+            </article>
+      </main>
   );
 }
 
--- 6 React/memegenerator/src/Main.js
+++ 6 React/memegenerator/src/Main.js
@@ -1,11 +1,26 @@
+import memesData from "./memesData"
+import {useState} from "react"
+
 function Main() {
+    // let newData = memesData.data.memes;
+    // let randomNum = Math.floor(Math.random()* newData.length);
+    let [memeImage, setMemeImage] = useState(``)
+
+    function getMemeImage(){
+        let newData = memesData.data.memes;
+        let randomNum = Math.floor(Math.random()* newData.length);
+        setMemeImage(newData[randomNum].url)
+    }
+    
     return (
         <main className="main">
-            <form>
+            <div className='form'>
                 <input type="text" placeholder="first text" />
                 <input type="text" placeholder="second text" />
-                <button className="btn">Get a new Meme image</button>
-            </form>
+                <button className="btn" onClick={getMemeImage} >Get a new Meme image</button>
+            </div>
+            {/* <img src={getMemeImage()} alt="meme Images" className="meme-img"/> */}
+            <img src={memeImage} alt="meme Images" className="meme-img"/>
         </main>
     )
 }
